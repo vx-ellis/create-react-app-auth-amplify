7768317059c4dc9c2c4960736988ab52da5e62e6
vx-ellis <ellis@vertexaccess.co.uk>
2020-10-24 21:27:52 +0100

    merged master

--- src/index.js
+++ src/index.js
@@ -4,7 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+class Root extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className = "Root">
+                Root element
+            </div>
+        )
+    }
+}
+
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
